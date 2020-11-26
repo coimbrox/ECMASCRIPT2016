ce473bd9c094a5420de8291a0d2beca5416dbeb8
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-11-26 03:24:03 -0300

    arrow function

---
+++ arrow_functions.js
@@ -0,0 +1,10 @@
+var dobroDoValor = numero => numero * 2
+
+
+console.log(dobroDoValor(7))
+
+// var dobroDoValor = function (numero) {
+//   return numero * 2
+// }
+// a function sai e entra arrow e se for um parametro só posso utilizar a sintaxe sem parenteses
+// caso seja uma função simples posso apagar o return e as chaves 
