65ab519382e7cdf420e7894247fb8cf5b5e23c00
Gabriel Coimbra <gabao.coimbra@gmail.com>
2020-11-26 03:09:50 -0300

    Let e Const

---
+++ variavel.js
@@ -0,0 +1,12 @@
+var serie = 'Friends'
+
+if (true) {
+  let serie = 'How i Meet You Mother é melhor'
+
+}
+
+console.log(serie)
+
+//let é utilizada em um determinado contexto, assumindo o escopo onde estiver
+//var é utilizada em um contexto global
+//const é quando receber um valor constante que não será modificada, não pode fazer atribuições pós o processamento
